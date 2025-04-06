5114e4069263de2cd6e39eee49dc45253d02e6bb
jeroen <jeroen.vanschijndel@otoy.com>
2025-04-06 16:08:38 +0200

    software tile. needs more work

--- src/main.c
+++ src/main.c
@@ -9,16 +9,53 @@
 
 u16 viewx = 0, viewy = 0;
 
-
+const uint8_t cross_tile[16] = {
+    0x00, 0x3C,  // Row 0
+    0x00, 0x3C,  // Row 1
+    0xFF, 0xFF,  // Row 2
+    0xFF, 0xFF,  // Row 3
+    0xFF, 0xFF,  // Row 4
+    0xFF, 0xFF,  // Row 5
+    0x00, 0x3C,  // Row 6
+    0x00, 0x3C   // Row 7
+};
 u8 pallette[]={170, 170,170,170,170,170,170,170};
+
+
+void vblank_handler()
+{
+	
+    set_bkg_tile_xy(5, 5, 0);  // example: put tile 42 at (5,5)
+}
 
 joypads_t joypads;
 void main(void)
 {
-	gotogxy(1, 2);
-	gprintf("X__");
+    DISPLAY_OFF;
+	// register interrupts
+    disable_interrupts();
+    add_VBL(vblank_handler);
+    enable_interrupts();
+
 	color(BLACK, WHITE, SOLID);
     draw_image(data_tiles);
+
+	const unsigned char checker_tile[] = {
+		0xAA, 0xAA,
+		0x55, 0x55,
+		0xAA, 0xAA,
+		0x55, 0x55,
+		0xAA, 0xAA,
+		0x55, 0x55,
+		0xAA, 0xAA,
+		0x55, 0x55,
+	};
+	// write to 0x8000
+	LCDC_REG |= 0x10;
+   	set_bkg_data(0, 1, checker_tile);
+
+
+    DISPLAY_ON;
 	while(1)
 	{
         // poll joypads
@@ -28,7 +65,7 @@ void main(void)
         if (joypads.joy0 & J_LEFT && viewx > 1)
 			viewx -= 1;
         if (joypads.joy0 & J_RIGHT && viewx < 0xffff - 2)
-			viewx += 1;		 
+			viewx += 1;
         if (joypads.joy0 & J_UP && viewy > 1)
 			viewy -= 1;
         if (joypads.joy0 & J_DOWN && viewy < 0xffff - 2)
@@ -36,10 +73,7 @@ void main(void)
 
 		renderer_setTarget(viewx, viewy);
 		renderer_render();
-
-		for(i16 i = 0; i < 16; i++)
-			_VRAM9000[i] = 16 + i;
 		
-		vsync();
+		wait_vbl_done();
 	}
 }
